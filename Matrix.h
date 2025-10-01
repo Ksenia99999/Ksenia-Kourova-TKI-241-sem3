01fdd44dabada72e8276cdb7a0263f47414861e5
Ksenia Kourova <146723050+Ksenia99999@users.noreply.github.com>
2025-10-01 15:20:08 +0300

    Update Matrix.h

--- Matrix.h
+++ Matrix.h
@@ -10,25 +10,20 @@ namespace miit_algebra
         std::vector<int> data;
 
     public:
-        // §¬§à§ß§ã§ä§â§å§Ü§ä§à§â§í §á§à §å§Þ§à§Ý§é§Ñ§ß§Ú§ð
         Matrix() = default;
         Matrix(const Matrix&) = default;
         Matrix(Matrix&&) = default;
         ~Matrix() = default;
 
-        // §°§á§Ö§â§Ñ§ä§à§â§í §á§â§Ú§ã§Ó§Ñ§Ú§Ó§Ñ§ß§Ú§ñ
         Matrix& operator=(const Matrix&) = default;
         Matrix& operator=(Matrix&&) = default;
 
-        // §°§á§Ö§â§Ñ§ä§à§â §Õ§à§ã§ä§å§á§Ñ §á§à §Ú§ß§Õ§Ö§Ü§ã§å
         int& operator[](size_t index);
         const int& operator[](size_t index) const;
 
-        // §°§á§Ö§â§Ñ§ä§à§â§í §ã§Õ§Ó§Ú§Ô§Ñ
         Matrix operator<<(int shift) const;
         Matrix operator>>(int shift) const;
 
-        // §°§ã§ß§à§Ó§ß§í§Ö §Þ§Ö§ä§à§Õ§í
         size_t size() const;
         std::string to_string() const;
         void resize(size_t size);
