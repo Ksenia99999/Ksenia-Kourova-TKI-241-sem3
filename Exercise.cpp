8870244e14f8370127673ca829e4ec767fdbe17d
Ksenia Kourova <146723050+Ksenia99999@users.noreply.github.com>
2025-10-16 14:12:40 +0300

    Update Exercise.cpp

--- Exercise.cpp
+++ Exercise.cpp
@@ -1,17 +1,23 @@
 #include "Exercise.h"
 
-namespace miit_algebra
+namespace MATRIX
 {
-    Exercise::Exercise(Generator* gen, size_t size) : generator(gen)
+    template <class Type>
+    Exercise<Type>::Exercise(Generator<Type>* gen, size_t rows, size_t columns)
+        : generator(gen), matrix(rows, columns)
     {
-        matrix.resize(size);
-        for (size_t i = 0; i < size; ++i) {
-            matrix[i] = generator->generate();
+        for (size_t i = 0; i < rows; ++i) {
+            for (size_t j = 0; j < columns; ++j) {
+                matrix(i, j) = generator->generate();
+            }
         }
     }
 
-    Matrix Exercise::get_matrix() const
+    template <class Type>
+    Matrix<Type> Exercise<Type>::get_matrix() const
     {
         return matrix;
     }
+
+    template class Exercise<int>;
 }
