a6e08b5abcda1d1cc9d10ed77b41caf7c4c0ac5d
Ksenia Kourova <146723050+Ksenia99999@users.noreply.github.com>
2025-10-30 10:47:46 +0300

    Update VariantExercise.h

--- VariantExercise.h
+++ VariantExercise.h
@@ -7,8 +7,8 @@ namespace MATRIX
     class VariantExercise : public Exercise<Type>
     {
     public:
-        VariantExercise(Generator<Type>* gen, size_t rows, size_t columns);
-
+        VariantExercise(Generator<Type>* gen, size_t size);
+        
         void Task1() override;
         void Task2() override;
         Matrix<Type> Task3() override;
