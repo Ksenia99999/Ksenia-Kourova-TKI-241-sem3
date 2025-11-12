a1ae63f041a3293c280310004ca2077af70f069a
Ksenia Kourova <146723050+Ksenia99999@users.noreply.github.com>
2025-11-12 13:55:07 +0300

    Update VariantExercise.cpp

--- VariantExercise.cpp
+++ VariantExercise.cpp
@@ -3,28 +3,70 @@
 namespace MATRIX
 {
     template <class Type>
-    VariantExercise<Type>::VariantExercise(Generator<Type>* gen, size_t size) 
-        : Exercise<Type>(gen, size) 
+    VariantExercise<Type>::VariantExercise(Generator<Type>* gen, size_t size)
+        : Exercise<Type>(gen, size)
     {
     }
-    
+
+    // Task1Exercise
     template <class Type>
-    void VariantExercise<Type>::Task1()
+    Task1Exercise<Type>::Task1Exercise(Generator<Type>* gen, size_t size)
+        : VariantExercise<Type>(gen, size)
+    {
+    }
+
+    template <class Type>
+    void Task1Exercise<Type>::execute()
+    {
+        Task1();
+    }
+
+    template <class Type>
+    void Task1Exercise<Type>::Task1()
     {
         this->array.replaceLastPositiveWithSecond();
     }
-    
+
+    // Task2Exercise
     template <class Type>
-    void VariantExercise<Type>::Task2()
+    Task2Exercise<Type>::Task2Exercise(Generator<Type>* gen, size_t size)
+        : VariantExercise<Type>(gen, size)
+    {
+    }
+
+    template <class Type>
+    void Task2Exercise<Type>::execute()
+    {
+        Task2();
+    }
+
+    template <class Type>
+    void Task2Exercise<Type>::Task2()
     {
         this->array.insertMaxBeforeOnes();
     }
-    
+
+    // Task3Exercise
     template <class Type>
-    Matrix<Type> VariantExercise<Type>::Task3()
+    Task3Exercise<Type>::Task3Exercise(Generator<Type>* gen, size_t size)
+        : VariantExercise<Type>(gen, size)
+    {
+    }
+
+    template <class Type>
+    void Task3Exercise<Type>::execute()
+    {
+        Task3();
+    }
+
+    template <class Type>
+    Matrix<Type> Task3Exercise<Type>::Task3()
     {
         return this->array.createNewArray();
     }
 
     template class VariantExercise<int>;
+    template class Task1Exercise<int>;
+    template class Task2Exercise<int>;
+    template class Task3Exercise<int>;
 }
