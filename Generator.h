ee10ee36d84b1747b8a8bbb79ef3ebce00240877
Ksenia Kourova <146723050+Ksenia99999@users.noreply.github.com>
2025-11-12 13:47:16 +0300

    Update Generator.h

--- Generator.h
+++ Generator.h
@@ -9,4 +9,15 @@ namespace MATRIX
         virtual ~Generator() = default;
         virtual Type generate() = 0;
     };
+
+    template <class Type>
+    class ConstGenerator : public Generator<Type>
+    {
+    private:
+        Type constantValue;
+        
+    public:
+        ConstGenerator(Type value) : constantValue(value) {}
+        Type generate() override { return constantValue; }
+    };
 }
