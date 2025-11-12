f0821cef7d8faf3fd3b9340877dc4a622eae9ff8
Ksenia Kourova <146723050+Ksenia99999@users.noreply.github.com>
2025-11-12 13:54:12 +0300

    Update VariantExercise.h

--- VariantExercise.h
+++ VariantExercise.h
@@ -3,14 +3,46 @@
 
 namespace MATRIX
 {
+    // Базовый класс для упражнений
     template <class Type>
     class VariantExercise : public Exercise<Type>
     {
     public:
         VariantExercise(Generator<Type>* gen, size_t size);
-        
+        void execute() override = 0;
+    };
+
+    // Конкретные реализации
+    template <class Type>
+    class Task1Exercise : public VariantExercise<Type>
+    {
+    public:
+        Task1Exercise(Generator<Type>* gen, size_t size);
+        void execute() override;
         void Task1() override;
+        void Task2() override {}
+        Matrix<Type> Task3() override { return Matrix<Type>(); }
+    };
+
+    template <class Type>
+    class Task2Exercise : public VariantExercise<Type>
+    {
+    public:
+        Task2Exercise(Generator<Type>* gen, size_t size);
+        void execute() override;
+        void Task1() override {}
         void Task2() override;
+        Matrix<Type> Task3() override { return Matrix<Type>(); }
+    };
+
+    template <class Type>
+    class Task3Exercise : public VariantExercise<Type>
+    {
+    public:
+        Task3Exercise(Generator<Type>* gen, size_t size);
+        void execute() override;
+        void Task1() override {}
+        void Task2() override {}
         Matrix<Type> Task3() override;
     };
 }
