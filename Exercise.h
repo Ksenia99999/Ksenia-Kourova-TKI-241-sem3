611be4cb5938a1e0fb2d497efc95be30b5b3887b
Ksenia Kourova <146723050+Ksenia99999@users.noreply.github.com>
2025-11-27 11:30:50 +0300

    Update Exercise.h

--- Exercise.h
+++ Exercise.h
@@ -14,11 +14,7 @@ namespace MATRIX
     public:
         Exercise(Generator<Type>* gen, size_t size);
         virtual ~Exercise() = default;
-
-        virtual void execute() = 0; 
-        virtual void Task1() = 0;
-        virtual void Task2() = 0;
-        virtual Matrix<Type> Task3() = 0;
+        virtual void execute() = 0;
 
         Matrix<Type> get_array() const;
     };
