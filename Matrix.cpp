547f0d9f0ff1a21a125534718fe33e99b287dda3
Ksenia Kourova <146723050+Ksenia99999@users.noreply.github.com>
2025-12-11 00:18:07 +0300

    Update Matrix.cpp

--- Matrix.cpp
+++ Matrix.cpp
@@ -1,32 +1,30 @@
 #include "Matrix.h"
-#include <cmath>
-#include <algorithm>
-#include <stdexcept>
+#include <cmath>        
+#include <algorithm>   
+#include <stdexcept>    // Для исключений
 
 namespace MATRIX
 {
-    // Конструкторы
+    // Конструктор по умолчанию
     template <class Type>
-    Matrix<Type>::Matrix()
-    {
-    }
-
+    Matrix<Type>::Matrix() {}
+    
+    // Конструктор с заданным размером
     template <class Type>
     Matrix<Type>::Matrix(const size_t size)
     {
         data.resize(size);
         for (size_t i = 0; i < size; ++i) {
-            data[i] = Type();
+            data[i] = Type();  // Инициализация значением по умолчанию
         }
     }
-
+    
+    // Конструктор копирования
     template <class Type>
     Matrix<Type>::Matrix(const Matrix& matrix)
-        : data(matrix.data)
-    {
-    }
-
-    // Операторы
+        : data(matrix.data) {}
+    
+    // Оператор присваивания
     template <class Type>
     Matrix<Type> Matrix<Type>::operator=(const Matrix& other)
     {
@@ -35,35 +33,38 @@ namespace MATRIX
         }
         return *this;
     }
-
+    
+    // Оператор сложения
     template <class Type>
     Matrix<Type> Matrix<Type>::operator+(const Matrix& other) const
     {
         if (data.size() != other.data.size()) {
             throw std::invalid_argument("Array sizes must match for addition");
         }
-
+        
         Matrix result(data.size());
         for (size_t i = 0; i < data.size(); ++i) {
             result.data[i] = data[i] + other.data[i];
         }
         return result;
     }
-
+    
+    // Оператор вычитания
     template <class Type>
     Matrix<Type> Matrix<Type>::operator-(const Matrix& other) const
     {
         if (data.size() != other.data.size()) {
             throw std::invalid_argument("Array sizes must match for subtraction");
         }
-
+        
         Matrix result(data.size());
         for (size_t i = 0; i < data.size(); ++i) {
             result.data[i] = data[i] - other.data[i];
         }
         return result;
     }
-
+    
+    // Умножение на скаляр
     template <class Type>
     Matrix<Type> Matrix<Type>::operator*(const double coef) const
     {
@@ -73,14 +74,15 @@ namespace MATRIX
         }
         return result;
     }
-
-    // Методы доступа
+    
+    // Получить размер массива
     template <class Type>
     size_t Matrix<Type>::getSize() const
     {
         return data.size();
     }
-
+    
+    // Оператор доступа по индексу (запись)
     template <class Type>
     Type& Matrix<Type>::operator[](size_t index)
     {
@@ -89,7 +91,8 @@ namespace MATRIX
         }
         return data[index];
     }
-
+    
+    // Оператор доступа по индексу (чтение)
     template <class Type>
     const Type& Matrix<Type>::operator[](size_t index) const
     {
@@ -98,8 +101,8 @@ namespace MATRIX
         }
         return data[index];
     }
-
-    // Методы для задач
+    
+    // ЗАДАЧА 1: Заменить последний положительный элемент на второй
     template <class Type>
     void Matrix<Type>::replaceLastPositiveWithSecond()
     {
@@ -108,7 +111,8 @@ namespace MATRIX
             data[lastPositive] = data[1];
         }
     }
-
+    
+    // ЗАДАЧА 2: Вставить максимум перед элементами с цифрой 1
     template <class Type>
     void Matrix<Type>::insertMaxBeforeOnes()
     {
@@ -119,12 +123,13 @@ namespace MATRIX
             }
         }
     }
-
+    
+    // ЗАДАЧА 3: Создать новый массив по правилу
     template <class Type>
     Matrix<Type> Matrix<Type>::createNewArray() const
     {
         Matrix result(data.size());
-
+        
         for (size_t i = 0; i < data.size(); ++i) {
             if ((i + 1) % 3 == 0) {
                 result.data[i] = static_cast<Type>(i) * data[i];
@@ -135,8 +140,8 @@ namespace MATRIX
         }
         return result;
     }
-
-    // Вспомогательные методы
+    
+    // Проверка наличия цифры 1 в числе
     template <class Type>
     bool Matrix<Type>::containsDigitOne(Type number) const
     {
@@ -147,7 +152,8 @@ namespace MATRIX
         }
         return false;
     }
-
+    
+    // Поиск последнего положительного элемента
     template <class Type>
     size_t Matrix<Type>::findLastPositive() const
     {
@@ -159,7 +165,8 @@ namespace MATRIX
         }
         return static_cast<size_t>(-1);
     }
-
+    
+    // Поиск максимального элемента
     template <class Type>
     Type Matrix<Type>::findMax() const
     {
@@ -171,14 +178,15 @@ namespace MATRIX
         }
         return maxVal;
     }
-
-    // Реализация дружественных функций
+    
+    // Дружественный оператор: скаляр * массив
     template <class Type>
     Matrix<Type> operator*(const double coef, const Matrix<Type>& other)
     {
         return other * coef;
     }
-
+    
+    // Дружественный оператор вывода в поток
     template <class Type>
     std::ostream& operator<<(std::ostream& os, const Matrix<Type>& other)
     {
@@ -187,8 +195,8 @@ namespace MATRIX
         }
         return os;
     }
-
-    template class Matrix<int>;
     
+    // Явные инстанциации для типа int
+    template class Matrix<int>;
     template std::ostream& operator<<(std::ostream& os, const Matrix<int>& other);
 }
