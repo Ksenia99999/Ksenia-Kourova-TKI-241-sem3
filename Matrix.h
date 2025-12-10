af2158a13c44f423e568c8e40465c7b5993a95c0
Ksenia Kourova <146723050+Ksenia99999@users.noreply.github.com>
2025-12-11 00:16:54 +0300

    Update Matrix.h

--- Matrix.h
+++ Matrix.h
@@ -4,49 +4,50 @@
 
 namespace MATRIX
 {
+    // Шаблонный класс для одномерного массива
     template <class Type>
     class Matrix
     {
     private:
-        std::vector<Type> data;
-
+        std::vector<Type> data;  // Внутреннее хранилище
+        
         // Вспомогательные методы
-        bool containsDigitOne(Type number) const;
-        size_t findLastPositive() const;
-        Type findMax() const;
-
+        bool containsDigitOne(Type number) const;  // Проверка цифры 1
+        size_t findLastPositive() const;           // Поиск последнего положительного
+        Type findMax() const;                      // Поиск максимума
+        
     public:
-        // Конструкторы и деструктор
-        Matrix();
-        Matrix(const size_t size);
-        Matrix(const Matrix& matrix);
-        ~Matrix() = default;
-
+        // Конструкторы
+        Matrix();                           
+        Matrix(const size_t size);          // С заданным размером
+        Matrix(const Matrix& matrix);       // Копирования
+        ~Matrix() = default;                // Деструктор
+        
         // Операторы
-        Matrix operator=(const Matrix& other);
-        Matrix operator+(const Matrix& other) const;
-        Matrix operator-(const Matrix& other) const;
-        Matrix operator*(const double coef) const;
-
+        Matrix operator=(const Matrix& other);     // Присваивания
+        Matrix operator+(const Matrix& other) const;  // Сложения
+        Matrix operator-(const Matrix& other) const;  // Вычитания
+        Matrix operator*(const double coef) const;    // Умножения на скаляр
+        
         // Дружественные операторы
         template <class T>
         friend Matrix<T> operator*(const double coef, const Matrix<T>& other);
         
         template <class T>
         friend std::ostream& operator<<(std::ostream& os, const Matrix<T>& other);
-
+        
         // Методы доступа
-        size_t getSize() const;
-        Type& operator[](size_t index);
-        const Type& operator[](size_t index) const;
-
-        // Методы для задач
-        void replaceLastPositiveWithSecond();
-        void insertMaxBeforeOnes();
-        Matrix createNewArray() const;
+        size_t getSize() const;                    // Получить размер
+        Type& operator[](size_t index);            // Доступ для записи
+        const Type& operator[](size_t index) const; // Доступ для чтения
+        
+        // Методы для трех задач варианта
+        void replaceLastPositiveWithSecond();  // Задача 1
+        void insertMaxBeforeOnes();            // Задача 2
+        Matrix createNewArray() const;         // Задача 3
     };
-
-    // Явные объявления дружественных функций
+    
+    // Глобальный оператор вывода
     template <class Type>
     std::ostream& operator<<(std::ostream& os, const Matrix<Type>& other);
 }
