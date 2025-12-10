b7e4564ea0cf4b9910cdece747ab46ddc7f3b1d4
Ksenia Kourova <146723050+Ksenia99999@users.noreply.github.com>
2025-12-11 00:19:51 +0300

    Update VariantExercise.h

--- VariantExercise.h
+++ VariantExercise.h
@@ -1,43 +1,44 @@
 #pragma once
-#include "Exercise.h"
+#include "Exercise.h" 
 
 namespace MATRIX
 {
+    // Промежуточный базовый класс
     template <class Type>
     class VariantExercise : public Exercise<Type>
     {
     public:
         VariantExercise(Generator<Type>* gen, size_t size);
     };
-
-    // Класс для ЗАДАЧИ 1
+    
+    // Класс для задачи 1
     template <class Type>
     class Task1Exercise : public VariantExercise<Type>
     {
     public:
         Task1Exercise(Generator<Type>* gen, size_t size);
-        void execute() override;  
+        void execute() override;
     };
-
-    // Класс для ЗАДАЧИ 2
+    
+    // Класс для задачи 2
     template <class Type>
     class Task2Exercise : public VariantExercise<Type>
     {
     public:
         Task2Exercise(Generator<Type>* gen, size_t size);
-        void execute() override;  
+        void execute() override;
     };
-
-    // Класс для ЗАДАЧИ 3
+    
+    // Класс для задачи 3 (сохраняет результат)
     template <class Type>
     class Task3Exercise : public VariantExercise<Type>
     {
     private:
-        Matrix<Type> task3Result;
-
+        Matrix<Type> task3Result;  // Хранение результата
+        
     public:
         Task3Exercise(Generator<Type>* gen, size_t size);
-        void execute() override; 
+        void execute() override;
         
         Matrix<Type> getTask3Result() const { return task3Result; }
     };
