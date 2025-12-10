934e98af7e71ed9d479f6de7ea12bc67f916bda1
Ksenia Kourova <146723050+Ksenia99999@users.noreply.github.com>
2025-12-11 00:16:09 +0300

    Update RandomGenerator.h

--- RandomGenerator.h
+++ RandomGenerator.h
@@ -1,18 +1,20 @@
 #pragma once
-#include "Generator.h"
-#include <random>
+#include "Generator.h" 
+#include <random>      
 
 namespace MATRIX
 {
+    // Генератор случайных чисел
     template <class Type>
     class RandomGenerator : public Generator<Type>
     {
     private:
-        std::mt19937 generator;
-        std::uniform_int_distribution<int> distribution;
-
+        std::mt19937 generator;  // Генератор Mersenne Twister
+        std::uniform_int_distribution<int> distribution;  // Равномерное распределение
+        
     public:
-        RandomGenerator(int min, int max);
-        Type generate() override;
+        RandomGenerator(int min, int max);  // Конструктор с диапазоном
+        
+        Type generate() override;  // Генерация случайного числа
     };
 }
