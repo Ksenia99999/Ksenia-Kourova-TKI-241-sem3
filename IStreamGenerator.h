02362ad3fb4aa434794f4078b5b0184410e93f0e
Ksenia Kourova <146723050+Ksenia99999@users.noreply.github.com>
2025-12-11 00:15:35 +0300

    Update IStreamGenerator.h

--- IStreamGenerator.h
+++ IStreamGenerator.h
@@ -1,17 +1,19 @@
 #pragma once
-#include "Generator.h"
-#include <iostream>
+#include "Generator.h" 
+#include <iostream>     
 
 namespace MATRIX
 {
+    // Генератор, читающий значения из потока ввода
     template <class Type>
     class IStreamGenerator : public Generator<Type>
     {
     private:
-        std::istream& in;
-
+        std::istream& in;  // Ссылка на поток ввода
+        
     public:
-        IStreamGenerator(std::istream& in = std::cin);
-        Type generate() override;
+        IStreamGenerator(std::istream& in = std::cin);  // По умолчанию std::cin
+        
+        Type generate() override;  // Чтение из потока
     };
 }
