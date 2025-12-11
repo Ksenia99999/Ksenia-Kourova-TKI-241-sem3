725ea330331e6b36499fc2d9e4997e739df704e5
Ksenia Kourova <146723050+Ksenia99999@users.noreply.github.com>
2025-12-11 13:15:15 +0300

    Update IStreamGenerator.h

--- IStreamGenerator.h
+++ IStreamGenerator.h
@@ -1,5 +1,3 @@
-// Генератор чисел из входного потока (например, с клавиатуры)
-// Считывает числа из std::cin или другого потока ввода
 #pragma once
 #include "Generator.h"
 #include <iostream>
@@ -10,7 +8,7 @@ namespace MATRIX
     class IStreamGenerator : public Generator
     {
     private:
-        std::istream& in;  // Поток ввода (обычно std::cin)
+        std::istream& in;  // Поток ввода 
 
     public:
         // Конструктор - принимает поток ввода
