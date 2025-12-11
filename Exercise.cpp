0534af24808e4457ee800133c29ba526dfd085b4
Ksenia Kourova <146723050+Ksenia99999@users.noreply.github.com>
2025-12-11 14:09:59 +0300

    Update Exercise.cpp

--- Exercise.cpp
+++ Exercise.cpp
@@ -1,24 +1,17 @@
 #include "Exercise.h"
 
-namespace MATRIX
+// Конструктор
+MATRIX::Exercise::Exercise(std::unique_ptr<Matrix> matrix, std::unique_ptr<Generator> generator)
+    : matrix(std::move(matrix)), generator(std::move(generator)) {}
+
+// Получение матрицы для записи
+MATRIX::Matrix& MATRIX::Exercise::get_matrix()
 {
-    // Конструктор Exercise - заполняет массив через генератор
-    template <class Type>
-    Exercise<Type>::Exercise(Generator<Type>* gen, size_t size)
-        : generator(gen), array(size)
-    {
-        for (size_t i = 0; i < size; ++i) {
-            array[i] = generator->generate();
-        }
-    }
-    
-    // Получить копию массива
-    template <class Type>
-    Matrix<Type> Exercise<Type>::get_array() const
-    {
-        return array;
-    }
-    
-    // Явная инстанциация для int
-    template class Exercise<int>;
+    return *matrix;
+}
+
+// Получение матрицы для чтения
+const MATRIX::Matrix& MATRIX::Exercise::get_matrix() const
+{
+    return *matrix;
 }
