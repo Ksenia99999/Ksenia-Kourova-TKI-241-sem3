1543d2980d5d5e756733111f76d8d9371c5dfded
Ksenia Kourova <146723050+Ksenia99999@users.noreply.github.com>
2025-12-11 10:46:57 +0300

    Update Matrix.h

--- Matrix.h
+++ Matrix.h
@@ -1,53 +1,55 @@
 #pragma once
 #include <iostream>
-#include <vector>
+#include <memory>
+#include <cmath>
+#include <stdexcept>
 
 namespace MATRIX
 {
-    // Шаблонный класс для одномерного массива
     template <class Type>
     class Matrix
     {
     private:
-        std::vector<Type> data;  // Внутреннее хранилище
-        
+        std::unique_ptr<Type[]> data;  // Умный указатель на массив
+        size_t arraySize;              // Размер массива
+
         // Вспомогательные методы
-        bool containsDigitOne(Type number) const;  // Проверка цифры 1
-        size_t findLastPositive() const;           // Поиск последнего положительного
-        Type findMax() const;                      // Поиск максимума
-        
+        bool containsDigitOne(Type number) const;
+        size_t findLastPositive() const;
+        Type findMax() const;
+
     public:
         // Конструкторы
-        Matrix();                           
-        Matrix(const size_t size);          // С заданным размером
-        Matrix(const Matrix& matrix);       // Копирования
-        ~Matrix() = default;                // Деструктор
-        
+        Matrix();
+        Matrix(const size_t size);
+        Matrix(const Matrix& matrix);
+        ~Matrix() = default;
+
         // Операторы
-        Matrix operator=(const Matrix& other);     // Присваивания
-        Matrix operator+(const Matrix& other) const;  // Сложения
-        Matrix operator-(const Matrix& other) const;  // Вычитания
-        Matrix operator*(const double coef) const;    // Умножения на скаляр
-        
+        Matrix& operator=(const Matrix& other);
+        Matrix operator+(const Matrix& other) const;
+        Matrix operator-(const Matrix& other) const;
+        Matrix operator*(const double coef) const;
+
         // Дружественные операторы
         template <class T>
         friend Matrix<T> operator*(const double coef, const Matrix<T>& other);
-        
+
         template <class T>
         friend std::ostream& operator<<(std::ostream& os, const Matrix<T>& other);
-        
+
         // Методы доступа
-        size_t getSize() const;                    // Получить размер
-        Type& operator[](size_t index);            // Доступ для записи
-        const Type& operator[](size_t index) const; // Доступ для чтения
-        
-        // Методы для трех задач варианта
-        void replaceLastPositiveWithSecond();  // Задача 1
-        void insertMaxBeforeOnes();            // Задача 2
-        Matrix createNewArray() const;         // Задача 3
+        size_t getSize() const;
+        Type& operator[](size_t index);
+        const Type& operator[](size_t index) const;
+
+        // Методы для трех задач
+        void replaceLastPositiveWithSecond();
+        void insertMaxBeforeOnes();
+        Matrix createNewArray() const;
     };
-    
-    // Глобальный оператор вывода
+
+    // Дружественные операторы
     template <class Type>
     std::ostream& operator<<(std::ostream& os, const Matrix<Type>& other);
 }
