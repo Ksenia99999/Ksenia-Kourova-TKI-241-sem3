2a291c1e632a4553588988a3182aed4d4fd5e5b0
Ksenia Kourova <146723050+Ksenia99999@users.noreply.github.com>
2025-12-11 13:08:11 +0300

    Update Generator.h

--- Generator.h
+++ Generator.h
@@ -1,27 +1,11 @@
-#pragma once  
+#pragma once
 
-namespace MATRIX 
+namespace MATRIX
 {
-    // Базовый абстрактный класс для генераторов
-    template <class Type>
     class Generator
     {
     public:
-        virtual ~Generator() = default;  // Виртуальный деструктор
-        
-        virtual Type generate() = 0;  // Чисто виртуальный метод генерации
-    };
-
-    // Генератор, возвращающий одно константное значение
-    template <class Type>
-    class ConstGenerator : public Generator<Type>
-    {
-    private:
-        Type constantValue;  // Хранимое значение
-        
-    public:
-        ConstGenerator(Type value) : constantValue(value) {}  // Конструктор
-        
-        Type generate() override { return constantValue; }  // Всегда возвращает одно значение
+        virtual ~Generator() = default;
+        virtual int generate() = 0;
     };
 }
