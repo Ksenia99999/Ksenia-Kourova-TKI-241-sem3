a4af2c02a9d04bbef84f6521b432880915ea2d5d
Ksenia Kourova <146723050+Ksenia99999@users.noreply.github.com>
2025-12-11 13:21:06 +0300

    Update Exercise.h

--- Exercise.h
+++ Exercise.h
@@ -1,23 +1,27 @@
 #pragma once
-#include "Matrix.h"    
-#include "Generator.h"  
+#include "Matrix.h"
+#include "Generator.h"
+#include <memory>
 
 namespace MATRIX
 {
-    // Базовый абстрактный класс для упражнений
-    template <class Type>
     class Exercise
     {
     protected:
-        Matrix<Type> array;        // Массив для работы
-        Generator<Type>* generator; // Указатель на генератор
-        
+        std::unique_ptr<Matrix> matrix;      // Указатель на матрицу
+        std::unique_ptr<Generator> generator; // Указатель на генератор
+
     public:
-        Exercise(Generator<Type>* gen, size_t size);  // Конструктор
-        virtual ~Exercise() = default;                // Виртуальный деструктор
+        // Конструктор - принимает матрицу и генератор
+        Exercise(std::unique_ptr<Matrix> matrix, std::unique_ptr<Generator> generator);
         
-        virtual void execute() = 0;  // Чисто виртуальный метод выполнения
+        virtual ~Exercise() = default;  // Виртуальный деструктор
         
-        Matrix<Type> get_array() const;  // Получить массив
+        // Основной метод выполнения упражнения
+        virtual void task() = 0;
+        
+        // Методы доступа к матрице
+        Matrix& get_matrix();                    // Для изменения матрицы
+        const Matrix& get_matrix() const;        // Для чтения матрицы
     };
 }
