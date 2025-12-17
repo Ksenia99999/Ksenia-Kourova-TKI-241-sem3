946dbb1dbe05c314b016f4a5d41ba6d219fda0b0
Ksenia Kourova <146723050+Ksenia99999@users.noreply.github.com>
2025-12-17 22:55:14 +0300

    Update Matrix.cpp

--- Matrix.cpp
+++ Matrix.cpp
@@ -1,223 +1,147 @@
 #include "Matrix.h"
+#include "Generator.h"
 #include <algorithm>
 #include <stdexcept>
 
 namespace MATRIX
 {
-    // Конструкторы
-    template <class Type>
-    Matrix<Type>::Matrix() : data(nullptr), arraySize(0) {}
+    // Конструктор по умолчанию
+    Matrix::Matrix() : size(0), data(nullptr) {}
 
-    template <class Type>
-    Matrix<Type>::Matrix(const size_t size) : arraySize(size)
+    // Конструктор с заданным размером
+    Matrix::Matrix(const size_t size) : size(size), data(std::make_unique<int[]>(size)) {}
+
+    // Конструктор с размером и значением
+    Matrix::Matrix(const size_t size, const int& value) : size(size), data(std::make_unique<int[]>(size))
     {
-        if (size > 0) {
-            data = std::make_unique<Type[]>(size);
-            for (size_t i = 0; i < size; ++i) {
-                data[i] = Type();  // Инициализация значением по умолчанию
-            }
-        } else {
-            data = nullptr;
+        for (size_t i = 0; i < size; ++i)
+        {
+            data[i] = value;
         }
     }
 
-    template <class Type>
-    Matrix<Type>::Matrix(const Matrix& matrix) : arraySize(matrix.arraySize)
+    // Конструктор копирования
+    Matrix::Matrix(const Matrix& other) : size(other.size), data(std::make_unique<int[]>(other.size))
     {
-        if (arraySize > 0) {
-            data = std::make_unique<Type[]>(arraySize);
-            for (size_t i = 0; i < arraySize; ++i) {
-                data[i] = matrix.data[i];
-            }
-        } else {
-            data = nullptr;
+        for (size_t i = 0; i < size; ++i)
+        {
+            data[i] = other.data[i];
         }
     }
 
-    // Оператор присваивания
-    template <class Type>
-    Matrix<Type>& Matrix<Type>::operator=(const Matrix& other)
+    // Конструктор перемещения
+    Matrix::Matrix(Matrix&& other) noexcept : size(other.size), data(std::move(other.data))
     {
-        if (this != &other) {
-            if (other.arraySize > 0) {
-                auto newData = std::make_unique<Type[]>(other.arraySize);
-                for (size_t i = 0; i < other.arraySize; ++i) {
-                    newData[i] = other.data[i];
-                }
-                data = std::move(newData);
-                arraySize = other.arraySize;
-            } else {
-                data = nullptr;
-                arraySize = 0;
+        other.size = 0;
+    }
+
+    // Оператор присваивания копированием
+    Matrix& Matrix::operator=(const Matrix& other)
+    {
+        if (this != &other)
+        {
+            auto temp = std::make_unique<int[]>(other.size);
+            for (size_t i = 0; i < other.size; ++i)
+            {
+                temp[i] = other.data[i];
             }
+            data = std::move(temp);
+            size = other.size;
         }
         return *this;
     }
 
-    // Арифметические операции
-    template <class Type>
-    Matrix<Type> Matrix<Type>::operator+(const Matrix& other) const
+    // Оператор присваивания перемещением
+    Matrix& Matrix::operator=(Matrix&& other) noexcept
     {
-        if (arraySize != other.arraySize) {
-            throw std::invalid_argument("Array sizes must match for addition");
+        if (this != &other)
+        {
+            data = std::move(other.data);
+            size = other.size;
+            other.size = 0;
         }
+        return *this;
+    }
 
-        Matrix result(arraySize);
-        for (size_t i = 0; i < arraySize; ++i) {
-            result.data[i] = data[i] + other.data[i];
-        }
+    // Оператор сдвига влево
+    Matrix Matrix::operator<<(int shift) const
+    {
+        Matrix result(*this);
+        result <<= shift;
         return result;
     }
 
-    template <class Type>
-    Matrix<Type> Matrix<Type>::operator-(const Matrix& other) const
+    // Оператор сдвига вправо
+    Matrix Matrix::operator>>(int shift) const
     {
-        if (arraySize != other.arraySize) {
-            throw std::invalid_argument("Array sizes must match for subtraction");
-        }
-
-        Matrix result(arraySize);
-        for (size_t i = 0; i < arraySize; ++i) {
-            result.data[i] = data[i] - other.data[i];
-        }
+        Matrix result(*this);
+        result >>= shift;
         return result;
     }
 
-    template <class Type>
-    Matrix<Type> Matrix<Type>::operator*(const double coef) const
+    // Оператор сдвига влево с присваиванием
+    Matrix& Matrix::operator<<=(int shift)
     {
-        Matrix result(arraySize);
-        for (size_t i = 0; i < arraySize; ++i) {
-            result.data[i] = static_cast<Type>(data[i] * coef);
+        if (size == 0) return *this;
+
+        shift = shift % static_cast<int>(size);
+        if (shift < 0) shift += size;
+
+        auto temp = std::make_unique<int[]>(size);
+        for (size_t i = 0; i < size; ++i)
+        {
+            temp[(i + shift) % size] = data[i];
         }
-        return result;
+        data = std::move(temp);
+        return *this;
     }
 
-    // Методы доступа
-    template <class Type>
-    size_t Matrix<Type>::getSize() const
+    // Оператор сдвига вправо с присваиванием
+    Matrix& Matrix::operator>>=(int shift)
     {
-        return arraySize;
+        return *this <<= -shift;
     }
 
-    template <class Type>
-    Type& Matrix<Type>::operator[](size_t index)
+    // Оператор доступа для записи
+    int& Matrix::operator[](const size_t index)
     {
-        if (index >= arraySize) {
-            throw std::out_of_range("Index out of range");
-        }
+        if (index >= size) throw std::out_of_range("Index out of range");
         return data[index];
     }
 
-    template <class Type>
-    const Type& Matrix<Type>::operator[](size_t index) const
+    // Оператор доступа для чтения
+    const int& Matrix::operator[](const size_t index) const
     {
-        if (index >= arraySize) {
-            throw std::out_of_range("Index out of range");
-        }
+        if (index >= size) throw std::out_of_range("Index out of range");
         return data[index];
     }
 
-    // Задача 1: Замена последнего положительного элемента
-    template <class Type>
-    void Matrix<Type>::replaceLastPositiveWithSecond()
+    // Получение размера матрицы
+    size_t Matrix::get_size() const
     {
-        if (arraySize <= 1) return;  // Нужен как минимум второй элемент
-        
-        size_t lastPositive = findLastPositive();
-        if (lastPositive != static_cast<size_t>(-1)) {
-            data[lastPositive] = data[1];  // Заменяем на второй элемент
+        return size;
+    }
+
+    // Преобразование матрицы в строку
+    std::string Matrix::to_string() const
+    {
+        std::ostringstream oss;
+        oss << "[";
+        for (size_t i = 0; i < size; ++i)
+        {
+            oss << data[i];
+            if (i < size - 1) oss << " ";
+        }
+        oss << "]";
+        return oss.str();
+    }
+
+    // Заполнение матрицы через генератор
+    void Matrix::fill(Generator& generator)
+    {
+        for (size_t i = 0; i < size; ++i)
+        {
+            data[i] = generator.generate();
         }
     }
-
-    // Задача 2: Вставка максимума перед элементами с цифрой 1
-    template <class Type>
-    void Matrix<Type>::insertMaxBeforeOnes()
-    {
-        if (arraySize == 0) return;
-        
-        Type maxVal = findMax();
-        for (size_t i = 0; i < arraySize; ++i) {
-            if (containsDigitOne(data[i])) {
-                data[i] = maxVal;
-            }
-        }
-    }
-
-    // Задача 3: Создание нового массива
-    template <class Type>
-    Matrix<Type> Matrix<Type>::createNewArray() const
-    {
-        Matrix result(arraySize);
-        for (size_t i = 0; i < arraySize; ++i) {
-            if ((i + 1) % 3 == 0) {
-                // Каждый третий элемент: M_i = i * P_i
-                result.data[i] = static_cast<Type>(i) * data[i];
-            } else {
-                // Остальные: M_i = -P_i * (i + 1)
-                result.data[i] = -data[i] * static_cast<Type>(i + 1);
-            }
-        }
-        return result;
-    }
-
-    // Вспомогательные методы
-    template <class Type>
-    bool Matrix<Type>::containsDigitOne(Type number) const
-    {
-        int num = static_cast<int>(std::abs(number));
-        if (num == 0) return false;
-        
-        while (num > 0) {
-            if (num % 10 == 1) return true;
-            num /= 10;
-        }
-        return false;
-    }
-
-    template <class Type>
-    size_t Matrix<Type>::findLastPositive() const
-    {
-        for (size_t i = arraySize; i > 0; --i) {
-            size_t index = i - 1;
-            if (data[index] > 0) {
-                return index;
-            }
-        }
-        return static_cast<size_t>(-1);
-    }
-
-    template <class Type>
-    Type Matrix<Type>::findMax() const
-    {
-        if (arraySize == 0) return Type();
-        
-        Type maxVal = data[0];
-        for (size_t i = 1; i < arraySize; ++i) {
-            if (data[i] > maxVal) {
-                maxVal = data[i];
-            }
-        }
-        return maxVal;
-    }
-
-    // Дружественные операторы
-    template <class Type>
-    Matrix<Type> operator*(const double coef, const Matrix<Type>& other)
-    {
-        return other * coef;
-    }
-
-    template <class Type>
-    std::ostream& operator<<(std::ostream& os, const Matrix<Type>& other)
-    {
-        for (size_t i = 0; i < other.arraySize; ++i) {
-            os << other.data[i] << " ";
-        }
-        return os;
-    }
-
-    // Явные инстанциации
-    template class Matrix<int>;
-    template std::ostream& operator<<(std::ostream& os, const Matrix<int>& other);
 }
